de0c20e8830a6e73f28822deefc5428761d82dcc
Gabriel Tan <gabtan99@gmail.com>
2020-05-21 08:21:34 +0800

    removed cors and bodyparser

--- api/app.js
+++ api/app.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const { ApolloServer, gql } = require('apollo-server-express');
-const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT ? process.env.PORT : 4000;
-
-app.use(cors());
 
 // Construct a schema, using GraphQL schema language
 const typeDefs = gql`
