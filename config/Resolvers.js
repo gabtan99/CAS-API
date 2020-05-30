1a821d275f26b3cc4ccd9f5578d8df3cd80c0a5f
Gabriel Tan <gabtan99@gmail.com>
2020-05-30 12:46:30 +0800

    feat: attempt bookmark mutation files

--- config/Resolvers.js
+++ config/Resolvers.js
@@ -5,12 +5,16 @@ const { Attempt } = require('../api/resolvers/Attempt.resolver');
 const { Bookmark } = require('../api/resolvers/Bookmark.resolver');
 const { UserMutations } = require('../api/mutations/User.mutation');
 const { QuizMutations } = require('../api/mutations/Quiz.mutation');
+const { BookmarkMutations } = require('../api/mutations/Bookmark.mutation');
+const { AttemptMutations } = require('../api/mutations/Attempt.mutation');
 
 module.exports = {
   Query,
   Mutation: {
     ...UserMutations,
     ...QuizMutations,
+    ...BookmarkMutations,
+    ...AttemptMutations,
   },
   User,
   Quiz,
