17c4c861ae3daabc170107dbaf8e17475a0c01e9
Gabriel Tan <gabtan99@gmail.com>
2020-06-02 19:01:40 +0800

    feat: search query

--- api/resolvers/PublicQuery.resolver.js
+++ api/resolvers/PublicQuery.resolver.js
@@ -30,6 +30,38 @@ const PublicQuery = {
       cursor: nextCursor,
     };
   },
+  search: async (_, { keyword, user_limit = 2, quiz_limit = 3 }) => {
+    const quiz_results = await Quiz.findAll({
+      where: {
+        is_public: true,
+        is_active: true,
+        title: { [Op.iLike]: '%' + keyword + '%' },
+      },
+      order: [['id', 'DESC']],
+      raw: true,
+    });
+
+    const user_results = await User.findAll({
+      where: {
+        [Op.or]: {
+          username: { [Op.iLike]: '%' + keyword + '%' },
+          full_name: { [Op.iLike]: '%' + keyword + '%' },
+        },
+      },
+    });
+
+    const has_more_quizzes = !!quiz_results[quiz_limit];
+    const has_more_users = !!user_results[quiz_limit];
+    const quizzes = quiz_results.slice(0, quiz_limit - 1);
+    const users = user_results.slice(0, user_limit - 1);
+
+    return {
+      quizzes,
+      users,
+      has_more_quizzes,
+      has_more_users,
+    };
+  },
 };
 
 module.exports = { PublicQuery };
--- api/resolvers/User.resolver.js
+++ api/resolvers/User.resolver.js
@@ -1,9 +1,29 @@
 const Quiz = require('../models/Quiz.model');
 const Bookmark = require('../models/Bookmark.model');
+const getPaginatedPayload = require('../util/getPaginatePayload');
 
 const User = {
-  quizzes: ({ id }) => Quiz.findAll({ where: { user_id: id } }),
-  bookmarks: ({ id }) => Bookmark.findAll({ where: { user_id: id } }),
+  quizzes: async ({ id }, { conditions }) => {
+    const { sort_column = 'id', sort_order = 'DESC', keyword = '', limit = 5, cursor } = conditions;
+
+    const results = await Quiz.findAll({
+      where: {
+        is_public: true,
+        is_active: true,
+        user_id: id,
+        title: { [Op.iLike]: '%' + keyword + '%' },
+      },
+      order: [[sort_column, sort_order]],
+      raw: true,
+    });
+
+    const { data, nextCursor } = getPaginatedPayload(results, limit, cursor);
+
+    return {
+      quizzes: data,
+      cursor: nextCursor,
+    };
+  },
 };
 
 module.exports = {
